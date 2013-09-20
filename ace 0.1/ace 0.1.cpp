82a49bea76887226189d90d10ce87bccbafe02bc
omelyanchikd <omelyanchikd@ukr.net>
2013-09-20 13:39:16 +0300

    mistake

--- ace 0.1/ace 0.1.cpp
+++ ace 0.1/ace 0.1.cpp
@@ -1,12 +1,11 @@
 // ace 0.1.cpp : Defines the entry point for the console application.
 //
-
-#include "stdafx.h"
+#pragma once
 
 #include "world.h"
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int _tmain(int argc), _TCHAR* argv[])
 {
 	srand(1);
 	int n = 2, m = 10, i = 3;
