a1a0e8d3901074cad3a075aeb072d93e421204cf
omelyanchikd <omelyanchikd@ukr.net>
2013-10-21 18:20:53 +0300

    intro qlearning

--- ace 0.1/qlearning.cpp
+++ ace 0.1/qlearning.cpp
@@ -5,7 +5,15 @@ qlearning::qlearning(void)
 {
 }
 
-
-qlearning::~qlearning(void)
+void qlearning::init(int state, int action)
 {
+	for (int i = 0; i < state * action; i++)
+	{
+		q.push_back(1.0 / (state * action));
+	}
+}
+
+void qlearning::update(int state, double reward)
+{
+	q[s_n * _action + _state] =  
 }
--- ace 0.1/qlearning.h
+++ ace 0.1/qlearning.h
@@ -7,7 +7,20 @@ using namespace std;
 class qlearning
 {
 public:
+	
 	qlearning(void);
-	~qlearning(void);
+	void init(int state, int action);
+	void update(int state, double reward);
+	int get_action();
+
+private:
+	
+	int _state;
+	int _action;
+	vector<double> q;
+	double learning_factor;
+	double discount_factor;
+	int s_n;
+	int a_n;
 };
 
